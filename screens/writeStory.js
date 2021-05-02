f79c9af6aec39a19fdb6ac5f2f30bf17c898e15f
disha1128 <74188656+disha1128@users.noreply.github.com>
2021-05-02 19:50:55 +0530

    Update writeStory.js

--- screens/writeStory.js
+++ screens/writeStory.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 //import {Header} from 'react-native-elements'
 //import { SafeAreaProvider } from 'react-native-safe-area-context'
 
@@ -8,11 +8,13 @@ export default class WriteStory extends React.Component {
   render(){
       return(
           <View style={styles.container}>
-       
+
 
             <TextInput placeholder='Title of the story' ></TextInput>
             <TextInput placeholder='author of the story' ></TextInput>
             <TextInput multiline={true} placeholder='write story'  ></TextInput>
+            <TouchableOpacity style={{width:150, height:30,backgroundColor:'pink', justifyContent:'center', alignItems:'center'}}></TouchableOpacity>
+
           </View>
         
       )
